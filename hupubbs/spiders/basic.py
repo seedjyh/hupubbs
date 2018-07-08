262ddc3743190e4999a4d33481213b5a4483a7ad
seedjyh <seedjyh@gmail.com>
2018-07-08 21:38:19 +0800

    修改：使用ItemLoader

--- hupubbs/spiders/basic.py
+++ hupubbs/spiders/basic.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from hupubbs.items import HupubbsItem
+from scrapy.loader import ItemLoader
+
 
 class BasicSpider(scrapy.Spider):
     name = 'basic'
@@ -8,9 +10,9 @@ class BasicSpider(scrapy.Spider):
     start_urls = ['https://bbs.hupu.com/china-soccer']
 
     def parse(self, response):
-        item = HupubbsItem()
-        item['forum_name'] = response.xpath('//*[@id="forumname"]/text()').extract()[0]
-        item['total_theme_count'] = response.xpath('//*[@class="pageright"]/text()').re('[0-9]+')[0]
-        item['total_reply_count'] = response.xpath('//*[@class="pageright"]/text()').re('[0-9]+')[1]
-        item['today_post_count'] = response.xpath('//*[@class="pageright"]/text()').re('[0-9]+')[2]
-        return item
+        l = ItemLoader(item=HupubbsItem(), response=response)
+        l.add_xpath('forum_name', '//*[@id="forumname"][1]/text()')
+        l.add_xpath('total_theme_count', '//*[@class="pageright"]/text()', re='共([0-9]+)主题')
+        l.add_xpath('total_reply_count', '//*[@class="pageright"]/text()', re='/([0-9]+)回复')
+        l.add_xpath('today_post_count', '//*[@class="pageright"]/text()', re='今日([0-9]+)帖')
+        return l.load_item()

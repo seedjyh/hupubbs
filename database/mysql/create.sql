8f8c9210613da36c833372f8f35eb8d3b11a2656
seedjyh <seedjyh@gmail.com>
2018-07-15 10:27:27 +0800

    修改：数据库中所有表都建立自增主键。`reply`表中的“论坛回帖ID”并不是全局唯一的，要和thread_id共同组成唯一性约束。

--- database/mysql/create.sql
+++ database/mysql/create.sql
@@ -5,30 +5,36 @@ USE `hupubbs`;
 
 -- 用户表user
 CREATE TABLE IF NOT EXISTS `user` (
-    `id` VARCHAR(32) NOT NULL,
+    `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+    `forum_id` VARCHAR(32) NOT NULL,
     `nickname` VARCHAR(128) NOT NULL,
     `prosign` VARCHAR(1024) NOT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`forum_id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
 
 -- 主题表thread
 CREATE TABLE IF NOT EXISTS `thread` (
-    `id` VARCHAR(32) NOT NULL,
-    `user_id` VARCHAR(32) NOT NULL,
+    `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+    `forum_id` VARCHAR(32) NOT NULL,
+    `user_id` bigint(20) NOT NULL,
     `post_time` TIMESTAMP NOT NULL,
     `title` VARCHAR(1024) NOT NULL,
     PRIMARY KEY (`id`),
+    UNIQUE KEY (`forum_id`),
     FOREIGN KEY (`user_id`) REFERENCES `user`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
 
 -- 回帖表reply
 CREATE TABLE IF NOT EXISTS `reply` (
-    `id` VARCHAR(32) NOT NULL,
-    `thread_id` VARCHAR(32) NOT NULL,
-    `user_id` VARCHAR(32) NOT NULL,
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `thread_id` bigint(20) NOT NULL,
+    `forum_id` VARCHAR(20) NOT NULL,
+    `user_id` bigint(20) NOT NULL,
     `post_time` TIMESTAMP NOT NULL,
     `i_like_sum` INT NOT NULL,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`thread_id`) REFERENCES `thread`(`id`),
-    FOREIGN KEY (`user_id`) REFERENCES `user`(`id`)
+    FOREIGN KEY (`user_id`) REFERENCES `user`(`id`),
+    UNIQUE KEY (`thread_id`, `forum_id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;

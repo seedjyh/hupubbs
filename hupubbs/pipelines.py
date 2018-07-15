0b4c664e8b8fb983384a054d895a175e18383545
seedjyh <seedjyh@gmail.com>
2018-07-16 06:57:35 +0800

    新增：增加根据item调用mysql的函数框架。

--- hupubbs/pipelines.py
+++ hupubbs/pipelines.py
@@ -6,6 +6,7 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 import pymysql
+from hupubbs.items import HupubbsUserItem, HupubbsPlateItem, HupubbsSubjectItem, HupubbsReplyItem
 
 class HupubbsPipeline(object):
     def open_spider(self, spider):
@@ -17,13 +18,33 @@ class HupubbsPipeline(object):
         print(item)
         return item
 
+
 class MySQLPipeline(object):
     def open_spider(self, spider):
         self.db = pymysql.connect("127.0.0.1", "root", "123456", "hupubbs")
+        self.cursor = self.db.cursor()
+
     def close_spider(self, spider):
         self.db.close()
+
     def process_item(self, item, spider):
-        cursor = self.db.cursor()
-        cursor.execute('select id, nickname, prosign from user')
-        for x in cursor.fetchall():
-            print(x)
+        switcher = {
+            HupubbsUserItem: self.process_user_item,
+            HupubbsSubjectItem: self.process_subject_item,
+            HupubbsReplyItem: self.process_reply_item,
+            HupubbsPlateItem: self.process_plate_item,
+        }
+        switcher.get(type(item), lambda: "")()
+        return item
+
+    def process_user_item(self, item, spider):
+        pass
+
+    def process_plate_item(self, item, spider):
+        pass
+
+    def process_subject_item(self, item, spider):
+        pass
+
+    def process_reply_item(self, item, spider):
+        pass
